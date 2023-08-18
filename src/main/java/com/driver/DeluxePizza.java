75e0fbc74b859564a7200bb95ce5e1da6035efb9
nsp1919 <110881782+nsp1919@users.noreply.github.com>
2023-08-18 20:11:06 +0530

    first commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,42 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
+    private String bill="";
+    private int total=0;
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+
+        if (isVeg) {
+            setPrice(300);
+            setExtratoppings(70);
+            total += getPrice() + getExtracheese() + getExtratoppings();
+        } else {
+            setPrice(400);
+            setExtratoppings(120);
+            total += getPrice() + getExtracheese() + getExtratoppings();
+        }
+    }
+    private boolean addbag=false;
+    @Override
+    public void addTakeaway() {
+        super.addTakeaway();
+        if (!addbag){
+            this.total+=getBag();
+            addbag=true;
+        }
+    }
+
+
+    @Override
+    public String getBill() {
+        this.bill=this.bill+"Base Price Of The Pizza: "+getPrice() +"\n";
+        this.bill=this.bill+"Extra Cheese Added: "+getExtracheese()+"\n";
+        this.bill=this.bill+"Extra Toppings Added: "+getExtratoppings()+"\n";
+        if (addbag)
+            this.bill=this.bill+"Paperbag Added: "+getBag()+"\n";
+        this.bill=this.bill+"Total Price: "+total;
+        return bill;
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,31 +4,136 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private String bill="";
+
+    private final int extracheese=80;
+
+    private int extratoppings;
+
+    private final int bag=20;
+    private int total=0;
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Boolean getVeg() {
+        return isVeg;
+    }
+
+    public void setVeg(Boolean veg) {
+        isVeg = veg;
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
+    }
+
+    public int getExtracheese() {
+        return extracheese;
+    }
+
+    public int getExtratoppings() {
+        return extratoppings;
+    }
+
+    public void setExtratoppings(int extratoppings) {
+        this.extratoppings = extratoppings;
+    }
+
+    public int getBag() {
+        return bag;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public boolean isAddextracheese() {
+        return addextracheese;
+    }
+
+    public void setAddextracheese(boolean addextracheese) {
+        this.addextracheese = addextracheese;
+    }
+
+    public boolean isAddextratoppings() {
+        return addextratoppings;
+    }
+
+    public void setAddextratoppings(boolean addextratoppings) {
+        this.addextratoppings = addextratoppings;
+    }
+
+    public boolean isAddbag() {
+        return addbag;
+    }
+
+    public void setAddbag(boolean addbag) {
+        this.addbag = addbag;
+    }
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+
+        if (isVeg){
+            price=300;
+            extratoppings=70;
+            total+=price;
+        }
+        else{
+            price=400;
+            extratoppings=80;
+            total+=price;
+        }
     }
 
     public int getPrice(){
         return this.price;
     }
 
+    private boolean addextracheese=false;
+    private boolean addextratoppings=false;
+    private boolean addbag=false;
     public void addExtraCheese(){
         // your code goes here
+        if (!addextracheese){
+            total+=extracheese;
+            addextracheese=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!addextratoppings){
+            total+=extratoppings;
+            addextratoppings=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (!addbag){
+            total+=bag;
+            addbag=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+
+        this.bill=this.bill+"Base Price Of The Pizza: "+price +"\n";
+        if (addextracheese)
+            this.bill=this.bill+"Extra Cheese Added: "+extracheese+"\n";
+        if (addextratoppings)
+            this.bill=this.bill+"Extra Toppings Added: "+extratoppings+"\n";
+        if (addbag)
+            this.bill=this.bill+"Paperbag Added: "+bag+"\n";
+        this.bill=this.bill+"Total Price: "+total;
         return this.bill;
     }
 }
